2f33d3ef80c8a8f73ba35ec25786bfd083f8ecf1
강희인 <dwoosung1@gmail.com>
2023-02-05 23:37:53 +0900

    update ItemRegistry

---
+++ src/main/java/com/example/springboot_shopping/Service/Itemservice.java
@@ -0,0 +1,48 @@
+package com.example.springboot_shopping.Service;
+
+import com.example.springboot_shopping.dto.ItemFormDto;
+import com.example.springboot_shopping.entity.Item;
+import com.example.springboot_shopping.entity.ItemImg;
+import com.example.springboot_shopping.repository.ItemImgRepository;
+import com.example.springboot_shopping.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class Itemservice {
+
+    private final ItemRepository itemRepository;
+    private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws  Exception {
+
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        for (int i =0; i<itemImgFileList.size(); i++) {
+
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+
+            if(i == 0) {
+
+                itemImg.setRepimgYn("Y");
+            }else {
+
+                itemImg.setRepimgYn("N");
+                itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+            }
+
+        }
+
+        return item.getId();
+    }
+
+}
--- src/main/java/com/example/springboot_shopping/controller/ItemController.java
+++ src/main/java/com/example/springboot_shopping/controller/ItemController.java
@@ -1,16 +1,57 @@
 package com.example.springboot_shopping.controller;
 
+import com.example.springboot_shopping.Service.Itemservice;
 import com.example.springboot_shopping.dto.ItemFormDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final Itemservice itemservice;
 
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model){
         model.addAttribute("itemFormDto", new ItemFormDto());
         return "item/itemForm";
+    }
+
+    @PostMapping(value = "/admin/item/new")
+    public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, Model model,
+                          @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList) {
+
+        if (bindingResult.hasErrors()) {
+
+            return "item/itemForm";
+        }
+
+        if (itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
+
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값입니다.");
+            return "item/itemForm";
+        }
+
+        try {
+
+            itemservice.saveItem(itemFormDto, itemImgFileList);
+        } catch (Exception e) {
+
+            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
+            return "item/itemForm";
+
+        }
+
+        return "redirect:/";
+
     }
 }
